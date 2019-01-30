07192850213c79f3979cea61e2ed6dbc520ba64a
mike <boimik87@gmail.com>
2019-01-30 12:58:33 +0000

    black box showing

---
+++ app.js
@@ -0,0 +1,6 @@
+// import React from 'react';
+// import { render } from 'react-dom';
+// // import './dragon.css';
+// import Dragon from './index.js';
+
+// render(<Dragon />, document.getElementById('root'));
--- index.js
+++ index.js
@@ -1,6 +1,45 @@
 import React from 'react';
 import { render } from 'react-dom';
+import './dragon.css';
 
-const App = () => <h1>Hello World</h1>;
+const CELL_SIZE = 20;
+const WIDTH = 800;
+const HEIGHT = 600;
+
+const App = () => (
+    <div>
+        <div>
+            <div
+                className="board"
+                style={{
+                    width: WIDTH,
+                    height: HEIGHT,
+                    backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`
+                }}
+            >
+                <h1>Hello</h1>
+            </div>
+        </div>
+    </div>
+);
+
+// class Dragon extends React.Components {
+//     render() {
+//         return (
+//             <div>
+//                 <div>
+//                     <div className="board">
+//                         style =
+//                         {{
+//                             width: WIDTH,
+//                             height: HEIGHT,
+//                             backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`
+//                         }}
+//                     </div>
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
 
 render(<App />, document.getElementById('root'));
